9ba0316f67fc064e032c57434170a6ac86bf0b77
Marcilio Cipriano <marciliocipriano@hotmail.com>
2018-03-18 13:04:41 -0300

    initial commit

---
+++ appSuggest/Controller/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  appSuggest
+//
+//  Created by admin on 18/03/2018.
+//  Copyright © 2018 MarcilioCipriano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "appCell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        return cell
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+//        tableView.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
